4df29f620cc0fdf18d6b55a87f9be5c587dd5add
Niko Rimac <niko.rimac@fer.unizg.hr>
2024-10-25 11:43:10 +0200

    Added basic login form

--- menZg/src/App.tsx
+++ menZg/src/App.tsx
@@ -1,11 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import LoginForm from "./LoginForm.tsx";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      
+      <LoginForm />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
