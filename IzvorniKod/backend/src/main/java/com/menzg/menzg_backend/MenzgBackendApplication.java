74956ff226e6ae484a935619e70bc44ea2015f13
nik813 <nikola.bacic@fer.hr>
2024-11-06 21:59:35 +0100

    dodao sam kostur beckenda

---
+++ IzvorniKod/backend/src/main/java/com/menzg/menzg_backend/MenzgBackendApplication.java
@@ -0,0 +1,13 @@
+package com.menzg.menzg_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenzgBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenzgBackendApplication.class, args);
+	}
+
+}
