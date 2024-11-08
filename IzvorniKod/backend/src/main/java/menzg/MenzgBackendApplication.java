c11b314a69bffb7788e644fee00fded952258030
Blaž Stužić <blaz.stuzic@fer.unizg.hr>
2024-11-08 22:33:39 +0100

    Promjena strukture

--- IzvorniKod/backend/src/main/java/menzg/MenzgBackendApplication.java
+++ IzvorniKod/backend/src/main/java/menzg/MenzgBackendApplication.java
@@ -1,4 +1,4 @@
-package com.menzg.menzg_backend;
+package menzg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- IzvorniKod/backend/src/test/java/menzg/MenzgBackendApplicationTests.java
+++ IzvorniKod/backend/src/test/java/menzg/MenzgBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.menzg.menzg_backend;
+package menzg;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
