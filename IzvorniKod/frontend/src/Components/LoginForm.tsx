28445bebdc1d0096dc551c0b91cf83a977e5ffc7
niko-rimac <niko.rimac@fer.unizg.hr>
2024-11-11 23:37:13 +0100

    Post method added to login

--- IzvorniKod/frontend/src/Components/LoginForm.tsx
+++ IzvorniKod/frontend/src/Components/LoginForm.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import axios from "axios";
 import { Button, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import "../styles/Login.css";
@@ -17,10 +19,33 @@ function LoginForm() {
     navigate("/menze");
   };
 
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    const form = event.currentTarget as HTMLFormElement;
+    const email = form.elements.namedItem("email") as HTMLInputElement;
+    const password = form.elements.namedItem("password") as HTMLInputElement;
+
+    try {
+      const response = await axios.post("http://localhost:8080/api/student", {
+        email: email.value,
+        password: password.value,
+      });
+      console.log(response.data);
+      // Handle successful login
+    } catch (error) {
+      console.error("Error logging in", error);
+      // Handle login error
+    }
+  };
+
   return (
     <div className="login-page">
       <div className="login-form-container">
-        <Form className="w-100" style={{ maxWidth: "400px" }}>
+        <Form
+          className="w-100"
+          style={{ maxWidth: "400px" }}
+          onSubmit={handleSubmit}
+        >
           <h1 className="login-title">Ulogiraj se!</h1>
           <Form.Group className="mb-4">
             <Form.Label className="login-label">E-mail</Form.Label>
@@ -29,6 +54,7 @@ function LoginForm() {
               size="lg"
               type="email"
               placeholder="Enter email"
+              name="email"
             />
           </Form.Group>
 
@@ -39,6 +65,7 @@ function LoginForm() {
               size="lg"
               type="password"
               placeholder="Password"
+              name="password"
             />
           </Form.Group>
 
