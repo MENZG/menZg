7f05a57cefd33533f2e6fb9b871711a9588864da
nik813 <nikola.bacic@fer.hr>
2024-11-11 22:41:38 +0100

    promjena1

--- IzvorniKod/backend/src/main/java/menzg/controller/KorisnikConstroller.java
+++ IzvorniKod/backend/src/main/java/menzg/controller/KorisnikConstroller.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import menzg.service.StudentService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")
 public class KorisnikConstroller {
 
 	@Autowired
--- IzvorniKod/backend/src/main/java/menzg/controller/MenzaController.java
+++ IzvorniKod/backend/src/main/java/menzg/controller/MenzaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,20 +16,20 @@ import menzg.service.MenzaService;
 
 @RestController
 @RequestMapping("/menza")
+@CrossOrigin(origins = "http://localhost:5173")
 public class MenzaController {
 
 	@Autowired
 	private MenzaService menzaService;
 
-
-	//vraca sve menze
+	// vraca sve menze
 	@GetMapping("")
 	public List<Menza> listMenzas() {
 		return menzaService.listAll();
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<Menza> getMenzaData(@PathVariable Long id){
+	public ResponseEntity<Menza> getMenzaData(@PathVariable Long id) {
 		Menza mz = menzaService.getMenzaData(id);
 
 		System.out.println("iz baze sam izvukao " + mz);
