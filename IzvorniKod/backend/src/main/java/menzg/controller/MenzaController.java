9e7fc462137012f0b5e2139fec0befc8372cf66b
nik813 <nikola.bacic@fer.hr>
2024-11-13 02:57:33 +0100

    dodane dodatne rute za radna vremena

--- IzvorniKod/backend/src/main/java/menzg/controller/MenzaController.java
+++ IzvorniKod/backend/src/main/java/menzg/controller/MenzaController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import menzg.model.Menza;
+import menzg.model.RadnoVrijeme;
 import menzg.service.MenzaService;
 
 @RestController
@@ -34,7 +35,7 @@ public class MenzaController {
 		System.out.println("zatrazio si sve informacije o menzi ------ ");
 		Menza mz = menzaService.getMenzaData(id);
 
-		System.out.println("iz baze sam izvukao " + mz);
+		// System.out.println("iz baze sam izvukao " + mz);
 
 		if (mz == null) {
 			System.out.println("nisam pronasao trazenu manzu");
@@ -43,5 +44,15 @@ public class MenzaController {
 
 		return new ResponseEntity<>(mz, HttpStatus.OK);
 
+	}
+
+	@GetMapping("/radnovrime/{id}")
+	public ResponseEntity<List<RadnoVrijeme>> getRadnoVrijeme(@PathVariable Long id) {
+		// Fetch the working hours for the given Menza id
+		Menza mz = menzaService.getMenzaData(id);
+
+		List<RadnoVrijeme> radnoVrijemeList = mz.getRadnaVremena();
+
+		return ResponseEntity.ok(radnoVrijemeList);
 	}
 }
