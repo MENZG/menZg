04ec66989f3b9cd67be6fce05e12620c9321490b
nik813 <nikola.bacic@fer.hr>
2024-11-14 22:19:47 +0100

    authcontroller

---
+++ IzvorniKod/backend/src/main/java/menzg/controller/AuthController.java
@@ -0,0 +1,28 @@
+package menzg.controller;
+
+import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+	@GetMapping("")
+	public void googleAuth(HttpServletResponse response) throws IOException {
+		response.sendRedirect("/oauth2/authorization/google");
+	}
+
+	@GetMapping("/google/callback")
+	public ResponseEntity<String> googleCallback(OAuth2AuthenticationToken authentication) {
+		// Ovdje možeš dohvatiti korisničke podatke
+		String userEmail = authentication.getPrincipal().getAttributes().get("email").toString();
+		return ResponseEntity.ok("Authenticated as: " + userEmail);
+	}
+}
