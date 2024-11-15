ed1cd8c829a4ce74d142a1119552706f44c1eb7b
nik813 <nikola.bacic@fer.hr>
2024-11-15 23:28:36 +0100

    auth

--- IzvorniKod/backend/src/main/java/menzg/config/SecurityConfig.java
+++ IzvorniKod/backend/src/main/java/menzg/config/SecurityConfig.java
@@ -1,101 +1,94 @@
 package menzg.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import menzg.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+
+import menzg.service.CustomOAuth2UserService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+	// ovdje ce nas vodit nakon autentifikacije
+	@Value("https://frontendmain.onrender.com/menze")
+	private String frontendUrl;
 
-    //ovdje ce nas vodit nakon autentifikacije
-    @Value("${progi.fronted.url}/menze")
-    private String frontendUrl;
+	private final CustomOAuth2UserService customOAuth2UserService;
 
-    private final CustomOAuth2UserService customOAuth2UserService;
+	public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+		this.customOAuth2UserService = customOAuth2UserService;
+	}
 
-    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
-        this.customOAuth2UserService = customOAuth2UserService;
-    }
+	// @Profile({"oauth-security"})
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http.csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**")).authorizeHttpRequests(auth -> {
+			auth.requestMatchers("/home").permitAll();
+			/* auth.requestMatchers("/h2-console/**").permitAll(); */
+			auth.anyRequest().authenticated();
+		}).headers(headers -> {
+			headers.frameOptions().sameOrigin();
+		}) // Omogućuje učitavanje u iframeu s iste domene
 
+				// .headers(headers ->
+				// headers.frameOptions().sameOrigin())//.frameOptions().sameOrigin()
+				.oauth2Login(oauth2 -> {
+					oauth2.userInfoEndpoint(
+							userInfoEndpoint -> userInfoEndpoint.userAuthoritiesMapper(this.authorityMapper()))
+							.successHandler((request, response, authentication) -> {
+								response.sendRedirect(frontendUrl);
+							});
+				})
+				// .exceptionHandling(handling -> handling.authenticationEntryPoint(new
+				// Http403ForbiddenEntryPoint()))
+				// .formLogin(Customizer.withDefaults()) /*ovo cemo kasnije koristit kad budemo
+				// imali vise profila */
+				.build();
+	}
 
-    //@Profile({"oauth-security"})
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        return http.csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/h2-console/**"))
-                .authorizeHttpRequests(auth -> {
-            auth.requestMatchers("/home").permitAll();
-           /* auth.requestMatchers("/h2-console/**").permitAll();*/
-            auth.anyRequest().authenticated();
-        })
-                .headers(headers -> {
-                    headers.frameOptions().sameOrigin();
-                }) // Omogućuje učitavanje u iframeu s iste domene
+	private GrantedAuthoritiesMapper authorityMapper() {
+		final SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
 
-                // .headers(headers -> headers.frameOptions().sameOrigin())//.frameOptions().sameOrigin()
-                .oauth2Login(oauth2 -> {
-                    oauth2
-                            .userInfoEndpoint(
-                                    userInfoEndpoint -> userInfoEndpoint.userAuthoritiesMapper(this.authorityMapper()))
-                            .successHandler(
-                                    (request, response, authentication) -> {
-                                        response.sendRedirect(frontendUrl);
-                                    });
-                })
-                //.exceptionHandling(handling -> handling.authenticationEntryPoint(new Http403ForbiddenEntryPoint()))
-                //.formLogin(Customizer.withDefaults()) /*ovo cemo kasnije koristit kad budemo imali vise profila */
-                .build();
-    }
+		authorityMapper.setDefaultAuthority("ROLE_USER");
 
+		return authorityMapper;
+	}
 
+	// umjesto Customizer.withDefaults()
+	// moze i ovo
+	/*
+	 * oauth2 -> { oauth2 .userInfoEndpoint( userInfoEndpoint ->
+	 * userInfoEndpoint.userAuthoritiesMapper(this.authorityMapper()))
+	 * .successHandler( (request, response, authentication) -> {
+	 * response.sendRedirect(frontendUrl); }); }
+	 */
+	// objasnjenje drugog
+	/*
+	 * Konfigurira OAuth2 autentifikaciju kada se korisnik prijavljuje putem OAuth2
+	 * protokola.
+	 * 
+	 * userInfoEndpoint(userInfoEndpoint ->
+	 * userInfoEndpoint.userAuthoritiesMapper(this.authorityMapper())): Postavlja
+	 * mapiranje korisničkih prava (userAuthoritiesMapper). Metoda authorityMapper()
+	 * je metoda iz trenutne klase koja prilagođava prava korisnika prema
+	 * informacijama koje pruža OAuth2 provajder (npr. Google).
+	 * 
+	 * successHandler((request, response, authentication) -> { ... }): Definira
+	 * ponašanje nakon uspješne autentifikacije. U ovom slučaju, nakon uspješne
+	 * prijave korisnik se preusmjerava na određeni frontendUrl.
+	 */
 
+	// kad se zatrazi ...../ ne trazi login, ali kad se zatraze bilo koe druge rute
+	// trazi se login
 
-    private GrantedAuthoritiesMapper authorityMapper() {
-        final SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
-
-        authorityMapper.setDefaultAuthority("ROLE_USER");
-
-        return authorityMapper;
-    }
-
-
-
-
-
-    //umjesto  Customizer.withDefaults()
-    //moze i ovo
-   /* oauth2 -> {
-        oauth2
-                .userInfoEndpoint(
-                        userInfoEndpoint -> userInfoEndpoint.userAuthoritiesMapper(this.authorityMapper()))
-                .successHandler(
-                        (request, response, authentication) -> {
-                            response.sendRedirect(frontendUrl);
-                        });
-    }*/
-    //objasnjenje drugog
-    /*Konfigurira OAuth2 autentifikaciju kada se korisnik prijavljuje putem OAuth2 protokola.
-
-    userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint.userAuthoritiesMapper(this.authorityMapper())):
-    Postavlja mapiranje korisničkih prava (userAuthoritiesMapper). Metoda authorityMapper() je metoda iz trenutne klase koja prilagođava prava korisnika prema informacijama koje pruža OAuth2 provajder (npr. Google).
-
-    successHandler((request, response, authentication) -> { ... }):
-    Definira ponašanje nakon uspješne autentifikacije. U ovom slučaju, nakon uspješne prijave korisnik se preusmjerava na određeni frontendUrl.*/
-
-    //kad se zatrazi ...../ ne trazi login, ali kad se zatraze bilo koe druge rute trazi se login
-
-    /*oauth2 -> oauth2
-                .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService)*/
+	/*
+	 * oauth2 -> oauth2 .userInfoEndpoint(userInfo ->
+	 * userInfo.userService(customOAuth2UserService)
+	 */
 }
