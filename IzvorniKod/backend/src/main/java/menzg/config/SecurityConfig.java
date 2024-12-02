925773791dd5e39fb26bc8abd6dc13e13b27c1f7
nik813 <nikola.bacic@fer.hr>
2024-12-02 23:53:21 +0100

    circular reference

--- IzvorniKod/backend/src/main/java/menzg/config/SecurityConfig.java
+++ IzvorniKod/backend/src/main/java/menzg/config/SecurityConfig.java
@@ -1,6 +1,3 @@
-package menzg.config;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -16,56 +13,37 @@ import menzg.service.CustomOAuth2UserService;
 @EnableWebSecurity
 public class SecurityConfig {
 
-	// Ovdje se definiraju frontend URL-i gdje se korisnik preusmjerava nakon
-	// uspješne prijave
-	@Value("${progi.frontend.url}/menze")
-	private String frontendUrl;
-
 	private final CustomOAuth2UserService customOAuth2UserService;
 
-	private final SecurityConfig globalCorsConfig;
-
-	public SecurityConfig(CustomOAuth2UserService customOAuth2UserService, SecurityConfig globalCorsConfig) {
+	public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
 		this.customOAuth2UserService = customOAuth2UserService;
-		this.globalCorsConfig = globalCorsConfig;
 	}
-
-	// Constructor Injection za CorsConfigurationSource i CustomOAuth2UserService
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		// Omogućavanje CORS-a
-		// http.cors(cors -> cors.configurationSource(corsConfigurationSource));
-		// http.cors(cors ->cors.disable());
-
-		http.cors(cors -> cors.configurationSource(globalCorsConfig.corsConfigurationSource()))
-
+		http.cors(cors -> cors.configurationSource(
+				request -> new org.springframework.web.cors.CorsConfiguration().applyPermitDefaultValues())) // You can
+																												// customize
+																												// the
+																												// CORS
+																												// configuration
+																												// here
 				.csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**")).authorizeRequests(auth -> {
-					// Definiramo da su svi zahtjevi zaštićeni, osim home rute
-					auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll(); // Dopušta OPTIONS zahtjeve
+					auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
 					auth.requestMatchers("/home").permitAll();
-					auth.anyRequest().authenticated(); // Zaštita svih drugih ruta
+					auth.anyRequest().authenticated();
 				})
-				// .headers(header -> header.disable())
-				// .headers(headers -> headers.frameOptions(frameOptionsConfig ->
-				// frameOptionsConfig.sameOrigin())) // Omogućava iframe učitavanje sa iste
-				// domene
-				.oauth2Login(oauth2 -> {
-					// Konfiguriramo OAuth2 login putem Google-a
-					oauth2.userInfoEndpoint(
-							userInfoEndpoint -> userInfoEndpoint.userAuthoritiesMapper(this.authorityMapper()))
-							.successHandler((request, response, authentication) -> {
-								// Nakon uspješne prijave korisnik se preusmjerava na frontend URL
-								response.sendRedirect(frontendUrl);
-							});
-				});
+				.oauth2Login(oauth2 -> oauth2
+						.userInfoEndpoint(
+								userInfoEndpoint -> userInfoEndpoint.userAuthoritiesMapper(this.authorityMapper()))
+						.successHandler((request, response, authentication) -> response
+								.sendRedirect("https://frontendservice-l0s1.onrender.com/menze")));
 		return http.build();
 	}
 
-	// Metoda koja postavlja mapiranje korisničkih prava nakon autentifikacije
 	private GrantedAuthoritiesMapper authorityMapper() {
 		final SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
-		authorityMapper.setDefaultAuthority("ROLE_USER"); // Definiramo osnovnu ulogu za korisnika
+		authorityMapper.setDefaultAuthority("ROLE_USER");
 		return authorityMapper;
 	}
 }
