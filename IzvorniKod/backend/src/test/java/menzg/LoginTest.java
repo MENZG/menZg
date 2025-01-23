4a095cdd19e7026adde1173cb7fc9f547e6cbde3
Eugen Kozomara <ek55118@fer.hr>
2025-01-23 22:02:43 +0100

    Dodajem kodove testova

---
+++ IzvorniKod/backend/src/test/java/menzg/LoginTest.java
@@ -0,0 +1,47 @@
+package menzg;
+
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNot.not;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.Keys;
+import java.util.*;
+
+public class LoginTest {
+    private WebDriver driver;
+    private Map<String, Object> vars;
+    JavascriptExecutor js;
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+        js = (JavascriptExecutor) driver;
+        vars = new HashMap<String, Object>();
+    }
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+    @Test
+    public void loginTest() {
+        driver.get("http://localhost:5173/login/student");
+        driver.manage().window().setSize(new Dimension(809, 816));
+        driver.findElement(By.cssSelector(".login-submit-btn")).click();
+        driver.findElement(By.cssSelector(".aZvCDf:nth-child(6) .pGzURd")).click();
+    }
+}
